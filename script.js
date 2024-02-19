db92555a8ba742603f31fa567df9e75c795b9cb9
Jonas Vieira <57574605+vieirajon@users.noreply.github.com>
2024-02-19 17:29:54 -0300

    script added

---
+++ script.js
@@ -0,0 +1,34 @@
+const searchInput = document.getElementsById("search-input");
+const resultArtist = document.getElementsById("result-artist");
+const resultPlaylist = document.getElementById("result-playlists");
+
+function requestApi(searchTerm) {
+  const url = "link?name_like=${searchTerm}";
+  fetch(url)
+    .then((response) => response.json())
+    .then((result) => displayResults(result));
+}
+
+function displayResults() {
+  resultPlaylist.classList.add("hidden");
+  const artistName = document.getElementById("artist-name");
+  const artistImage = document.getElementById("artist-img");
+
+  result.forEach((element) => {
+    artistName.innerText = element.name;
+    artistImage.src = element.urlImg;
+  });
+
+  resultArtist.classList.remove("hidden");
+}
+
+document.addEventListener("input", function () {
+  const searchTerm = searchInput.value.roLowerCase();
+  if (searchTerm === "") {
+    resultPlaylist.classList.add("hidden");
+    resultArtist.classList.remove("hidden");
+    return;
+  }
+
+  requestApi(searchTerm);
+});
